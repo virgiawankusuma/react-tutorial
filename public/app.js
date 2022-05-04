4ccee8cb788e91070c07145e1f67eaa3f543f4f0
virgiawankusuma <virgiawan150@gmail.com>
2022-05-05 00:46:12 +0700

    [Basic React] Making counter app with react useState

--- public/app.js
+++ public/app.js
@@ -1,7 +1,18 @@
 const root = document.querySelector('#root');
 
 function App() {
-  return /*#__PURE__*/React.createElement("h1", null, "React Element");
+  const state = React.useState(0);
+  const count = state[0];
+  const updateCount = state[1];
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("button", {
+    onClick: function () {
+      updateCount(count - 1);
+    }
+  }, " - "), /*#__PURE__*/React.createElement("span", null, count), /*#__PURE__*/React.createElement("button", {
+    onClick: function () {
+      updateCount(count + 1);
+    }
+  }, " + "));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 const root = document.querySelector('#root');
 
+
 function App() {
-    return <h1>React Element</h1>
+    const state = React.useState(0);
+
+    const count = state[0];
+    const updateCount = state[1];
+
+    return (
+        <>
+            <button onClick={
+                function () {
+                    updateCount(count - 1);
+                }
+            }> - </button>
+            <span>{count}</span>
+            <button onClick={
+                function () {
+                    updateCount(count + 1);
+                }
+            }> + </button>
+        </>
+    )
 }
 
 ReactDOM.render(<App />, root);
