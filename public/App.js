725631861cb51af8eba56655d0e0e20f592c4c0b
virgiawankusuma <virgiawan150@gmail.com>
2022-07-27 15:55:22 +0700

    [The Essentials] Conditional rendering 1

--- public/App.js
+++ public/App.js
@@ -1,52 +1,17 @@
 const root = document.querySelector('#root');
 
 function App() {
-  const [diklik, setDiklik] = React.useState(false);
-  const [count, setCount] = React.useState(0);
-  /**
-   * useEffect all state
-  React.useEffect(
-    function () {
-      console.log("exec");
-    }
-  );
-   */
+  const [login, setLogin] = React.useState(false);
 
-  /**
-   * useEffect specific state
-  React.useEffect(
-    function () {
-      console.log("exec");
-    },
-    [diklik]
-  );
-   */
+  if (login) {
+    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Udah login"), /*#__PURE__*/React.createElement("button", {
+      onClick: setLogin.bind(this, false)
+    }, "Logout"));
+  }
 
-  /**
-   * useEffect initialize 1 time using empty array
-  React.useEffect(
-    function () {
-      console.log("exec");
-    },
-    [diklik]
-  );
-   */
-
-  /** destroy when mount new component aka Cleanup */
-
-  React.useEffect(function () {
-    console.log('init carousel');
-    return function () {
-      console.log('destroy');
-    };
-  });
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
-    id: "judul"
-  }, "Ini adalah judul"), /*#__PURE__*/React.createElement("button", {
-    onClick: setDiklik.bind(this, true)
-  }, "Klik aku"), /*#__PURE__*/React.createElement("button", {
-    onClick: setCount.bind(this, count + 1)
-  }, "Tambah"), /*#__PURE__*/React.createElement("span", null, count));
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Login dulu bang"), /*#__PURE__*/React.createElement("button", {
+    onClick: setLogin.bind(this, true)
+  }, "Login"));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/App.js
+++ src/App.js
@@ -1,53 +1,21 @@
 const root = document.querySelector('#root');
 
 function App() {
-  const [diklik, setDiklik] = React.useState(false);
-  const [count, setCount] = React.useState(0);
+  const [login, setLogin] = React.useState(false);
 
-  /**
-   * useEffect all state
-  React.useEffect(
-    function () {
-      console.log("exec");
-    }
-  );
-   */
-
-  /**
-   * useEffect specific state
-  React.useEffect(
-    function () {
-      console.log("exec");
-    },
-    [diklik]
-  );
-   */
-
-  /**
-   * useEffect initialize 1 time using empty array
-  React.useEffect(
-    function () {
-      console.log("exec");
-    },
-    [diklik]
-  );
-   */
-
-  /** destroy when mount new component aka Cleanup */
-  React.useEffect(function () {
-    console.log('init carousel');
-
-    return function () {
-      console.log('destroy');
-    };
-  });
+  if (login) {
+    return (
+      <>
+        <h1>Udah login</h1>
+        <button onClick={setLogin.bind(this, false)}>Logout</button>
+      </>
+    );
+  }
 
   return (
     <>
-      <h1 id='judul'>Ini adalah judul</h1>
-      <button onClick={setDiklik.bind(this, true)}>Klik aku</button>
-      <button onClick={setCount.bind(this, count + 1)}>Tambah</button>
-      <span>{count}</span>
+      <h1>Login dulu bang</h1>
+      <button onClick={setLogin.bind(this, true)}>Login</button>
     </>
   );
 }
