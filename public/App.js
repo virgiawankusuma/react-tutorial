4b4bc3ac6491f7c42aa81fbd0f55ff7cf2b42152
virgiawankusuma <virgiawan150@gmail.com>
2022-07-28 15:14:02 +0700

    [The Essentials] Sync data fetching response with UI

--- public/App.js
+++ public/App.js
@@ -1,16 +1,26 @@
 const root = document.querySelector('#root');
 
 function App() {
+  const [news, setNews] = React.useState([]);
+  const [loading, setLoading] = React.useState([true]);
   React.useEffect(function () {
     async function getData() {
       const request = await fetch('https://api.spaceflightnewsapi.net/v3/blogs'); // console.log(request);
 
-      const response = await request.json();
-      console.log(response);
+      const response = await request.json(); // console.log(response);
+
+      setNews(response);
+      setLoading(false);
     }
 
     getData();
   }, []);
+  return /*#__PURE__*/React.createElement(React.Fragment, null, loading ? /*#__PURE__*/React.createElement("b", null, "Lagi loading..") : /*#__PURE__*/React.createElement("ul", null, news.map(item => {
+    console.log(item);
+    return /*#__PURE__*/React.createElement("li", {
+      key: item.id
+    }, item.title);
+  })));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 const root = document.querySelector('#root');
 
 function App() {
+  const [news, setNews] = React.useState([]);
+  const [loading, setLoading] = React.useState([true]);
+
   React.useEffect(function () {
     async function getData() {
       const request = await fetch(
@@ -9,10 +12,27 @@ function App() {
       // console.log(request);
       const response = await request.json();
 
-      console.log(response);
+      // console.log(response);
+      setNews(response);
+      setLoading(false);
     }
     getData();
   }, []);
+
+  return (
+    <>
+      {loading ? (
+        <b>Lagi loading..</b>
+      ) : (
+        <ul>
+          {news.map((item) => {
+            console.log(item);
+            return <li key={item.id}>{item.title}</li>;
+          })}
+        </ul>
+      )}
+    </>
+  );
 }
 
 ReactDOM.render(<App />, root);
