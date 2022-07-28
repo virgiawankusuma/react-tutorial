97c8c1515a9117be40b46249267ff03eb3917c89
virgiawankusuma <virgiawan150@gmail.com>
2022-07-28 13:27:50 +0700

    [The Essentials] Data fetching (HTTP client request) using Promise

--- public/App.js
+++ public/App.js
@@ -1,26 +1,15 @@
 const root = document.querySelector('#root');
 
 function App() {
-  const [nama, setName] = React.useState('virgiawan');
-
-  function ketikAkuDisubmit(event) {
-    event.preventDefault();
-    console.log('Nama: ', nama);
-  }
-
-  return /*#__PURE__*/React.createElement("form", {
-    onSubmit: ketikAkuDisubmit
-  }, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Nama: "), /*#__PURE__*/React.createElement("input", {
-    type: "text",
-    name: "nama",
-    value: nama,
-    onChange: function (e) {
-      console.log(e.target.value);
-      setName(e.target.value);
-    }
-  })), /*#__PURE__*/React.createElement("button", {
-    type: "submit"
-  }, "Kirim"));
+  React.useEffect(function () {
+    const getData = fetch('https://api.spaceflightnewsapi.net/v3/blogs').then(resp => {
+      // console.log(resp);
+      // console.log(resp.json());
+      return resp.json();
+    }).then(resp => {
+      console.log(resp);
+    });
+  }, []);
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 const root = document.querySelector('#root');
 
 function App() {
-  const [nama, setName] = React.useState('virgiawan');
-
-  function ketikAkuDisubmit(event) {
-    event.preventDefault();
-
-    console.log('Nama: ', nama);
-  }
-
-  return (
-    <form onSubmit={ketikAkuDisubmit}>
-      <div>
-        <label>Nama: </label>
-        <input
-          type='text'
-          name='nama'
-          value={nama}
-          onChange={function (e) {
-            console.log(e.target.value);
-            setName(e.target.value);
-          }}
-        ></input>
-      </div>
-      <button type='submit'>Kirim</button>
-    </form>
-  );
+  React.useEffect(function () {
+    const getData = fetch('https://api.spaceflightnewsapi.net/v3/blogs')
+      .then((resp) => {
+        // console.log(resp);
+        // console.log(resp.json());
+        return resp.json();
+      })
+      .then((resp) => {
+        console.log(resp);
+      });
+  }, []);
 }
 
 ReactDOM.render(<App />, root);
