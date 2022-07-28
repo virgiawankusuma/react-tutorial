b6ba89e08806d9c6f62ba3cdf52724bfa347eaeb
virgiawankusuma <virgiawan150@gmail.com>
2022-07-28 15:01:49 +0700

    [The Essentials] Data fetching (HTTP client request) using Await

--- public/App.js
+++ public/App.js
@@ -1,38 +1,16 @@
 const root = document.querySelector('#root');
 
 function App() {
-<<<<<<< HEAD
   React.useEffect(function () {
-    const getData = fetch('https://api.spaceflightnewsapi.net/v3/blogs').then(resp => {
-      // console.log(resp);
-      // console.log(resp.json());
-      return resp.json();
-    }).then(resp => {
-      console.log(resp);
-    });
-  }, []);
-=======
-  const [nama, setName] = React.useState('virgiawan');
+    async function getData() {
+      const request = await fetch('https://api.spaceflightnewsapi.net/v3/blogs'); // console.log(request);
 
-  function ketikAkuDisubmit(event) {
-    event.preventDefault();
-    console.log('Nama: ', nama);
-  }
-
-  return /*#__PURE__*/React.createElement("form", {
-    onSubmit: ketikAkuDisubmit
-  }, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Nama: "), /*#__PURE__*/React.createElement("input", {
-    type: "text",
-    name: "nama",
-    value: nama,
-    onChange: function (e) {
-      console.log(e.target.value);
-      setName(e.target.value);
+      const response = await request.json();
+      console.log(response);
     }
-  })), /*#__PURE__*/React.createElement("button", {
-    type: "submit"
-  }, "Kirim"));
->>>>>>> 9799e7d64967c61449ff8bcbf52c7327854d189b
+
+    getData();
+  }, []);
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
 const root = document.querySelector('#root');
 
 function App() {
-<<<<<<< HEAD
   React.useEffect(function () {
-    const getData = fetch('https://api.spaceflightnewsapi.net/v3/blogs')
-      .then((resp) => {
-        // console.log(resp);
-        // console.log(resp.json());
-        return resp.json();
-      })
-      .then((resp) => {
-        console.log(resp);
-      });
+    async function getData() {
+      const request = await fetch(
+        'https://api.spaceflightnewsapi.net/v3/blogs'
+      );
+      // console.log(request);
+      const response = await request.json();
+
+      console.log(response);
+    }
+    getData();
   }, []);
-=======
-  const [nama, setName] = React.useState('virgiawan');
-
-  function ketikAkuDisubmit(event) {
-    event.preventDefault();
-
-    console.log('Nama: ', nama);
-  }
-
-  return (
-    <form onSubmit={ketikAkuDisubmit}>
-      <div>
-        <label>Nama: </label>
-        <input
-          type='text'
-          name='nama'
-          value={nama}
-          onChange={function (e) {
-            console.log(e.target.value);
-            setName(e.target.value);
-          }}
-        ></input>
-      </div>
-      <button type='submit'>Kirim</button>
-    </form>
-  );
->>>>>>> 9799e7d64967c61449ff8bcbf52c7327854d189b
 }
 
 ReactDOM.render(<App />, root);
