3f3242e3a09aaf2b82c3f60e5bd9c5ba1a479eb9
virgiawankusuma <virgiawan150@gmail.com>
2022-08-06 15:45:16 +0700

    [The Toolchain] Init React project with CRA

--- src/App.js
+++ src/App.js
@@ -1,154 +1,25 @@
-const root = document.querySelector('#root');
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [activity, setActivity] = React.useState('');
-  const [edit, setEdit] = React.useState({});
-  const [todos, setTodos] = React.useState([]);
-  const [message, setMessage] = React.useState('');
-
-  function generateId() {
-    return Date.now();
-  }
-
-  function saveTodoHandler(e) {
-    e.preventDefault();
-    // console.log(activity);
-
-    // empty field validation 
-    if (!activity) return setMessage('The field can\'t be blank');
-
-    setMessage('');
-    
-    if(edit.id) {
-      // console.log('edit');
-      const updatedTodo = {
-        // id: edit.id,
-        ...edit,
-        activity
-      }
-
-      const editTodoIndex = todos.findIndex((todo) => {
-        return todo.id == edit.id;
-      });
-
-      const updatedTodos = [...todos];
-      updatedTodos[editTodoIndex] = updatedTodo;
-      // console.log(updatedTodo);
-
-      setTodos(updatedTodos);
-
-      return discardEditHandler();
-    }
-    setTodos([
-      ...todos,
-      {
-        id: generateId(),
-        activity,
-        // activity: activity,
-        done: false
-      },
-    ]);
-    setActivity('');
-    // console.log(todos);
-  }
-
-  function removeTodoHandler(todoId) {
-    // console.log(todoId);
-    const filteredTodos = todos.filter((todo) => {
-      // console.log(todoId, todo.id);
-      return todo.id !== todoId;
-    });
-
-    // console.log(filteredTodos);
-    setTodos(filteredTodos);
-
-    // discardEditHandler();
-    edit.id && discardEditHandler();
-  }
-
-  function editTodoHandler(todo) {
-    // console.log(todo);
-    setActivity(todo.activity);
-    setEdit(todo);
-  }
-
-  function discardEditHandler() {
-    // console.log('discard edit');
-    setEdit({}); // for clear edit.id
-    setActivity('');
-  }
-
-  function doneTodoHandler(todo) {
-    // console.log(todo);
-    const updatedTodo = {
-      // id: todo.id,
-      // activity,
-      ...todo,
-      done: todo.done ? false : true,
-    }
-
-    // return console.log(updatedTodo);
-
-    const editTodoIndex = todos.findIndex((currentTodo) => currentTodo.id == todo.id);
-
-    const updatedTodos = [...todos];
-    updatedTodos[editTodoIndex] = updatedTodo;
-    // console.log(updatedTodo);
-
-    setTodos(updatedTodos);
-  }
-
   return (
-    <>
-      <h1>Simple To-Do List</h1>
-      <form onSubmit={saveTodoHandler}>
-        {
-          message &&
-          <span style={{
-            color: 'red',
-            display: 'block'
-          }}>{message}</span>
-        }
-        <input
-          type='text'
-          placeholder='Activity name..'
-          value={activity}
-          onChange={(e) => {
-            setActivity(e.target.value);
-          }}
-        />
-        <button type='submit'>
-          {edit.id ? 'Update' : 'Add'}
-        </button>
-        {
-          edit.id && <button type='button' onClick={discardEditHandler}>Discard</button>
-        }
-      </form>
-
-      { todos.length > 0 ? (
-        <ul>
-          {todos.map((todo) => {
-            return (
-              <li key={todo.id}>
-                <input 
-                  type="checkbox"
-                  checked={todo.done}
-                  onChange={doneTodoHandler.bind(this, todo)}
-                />
-                {todo.activity} ({todo.done ? 'Done' : 'Undone'})
-                <button onClick={editTodoHandler.bind(this, todo)}>
-                  Edit
-                </button>
-                <button onClick={removeTodoHandler.bind(this, todo.id)}>
-                  Hapus
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      ) : <p><em>Todo's is still empty</em></p>}
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
-ReactDOM.render(<App />, root);
+export default App;
