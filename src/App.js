e4a56c6d5e728a664a5821186294e72c8fc3271c
virgiawankusuma <virgiawan150@gmail.com>
2022-08-08 00:03:39 +0700

    [The Toolchain] Static file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import Button from './Button';
-import { Welcomeh2 } from './Welcome';
 
 function App() {
   return (
     <div className="App">
-      <Welcomeh2 children="welcome 1"/>
-      <Welcomeh2 children="welcome 2" />
-      <Welcomeh2>Saya adalah content</Welcomeh2>
-      <Button>Klik gue</Button>
+      <img src="/logo512.png" alt="logo reactjs"/>
     </div>
   );
 }
