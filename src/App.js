9afff339dd12e356e764d9c035f53eedd40a8455
virgiawankusuma <virgiawan150@gmail.com>
2022-08-07 23:51:04 +0700

    [The Toolchain] React component (separation of concerns)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { Welcomeh2 } from './Welcome';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcomeh2 children="welcome 1"/>
+      <Welcomeh2 children="welcome 2" />
+      <Welcomeh2>Saya adalah content</Welcomeh2>
     </div>
   );
 }
---
+++ src/Welcome.js
@@ -0,0 +1,8 @@
+export function Welcomeh2(props) {
+  console.log(props);
+  return <h2>{props.children}</h2>
+}
+
+export function Welcomeh3() {
+  return <h3>Welcome</h3>
+}
