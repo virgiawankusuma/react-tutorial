483c202ec5117b6b097456033eb6d0ddecfb7fbb
virgiawankusuma <virgiawan150@gmail.com>
2022-08-08 00:59:47 +0700

    [The Router] Basic routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import About from './About';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <img src="/logo512.png" alt="logo reactjs"/>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
