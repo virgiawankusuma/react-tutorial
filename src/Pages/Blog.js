b4c19ecc64592c365465dddf5510c86224c2b58d
virgiawankusuma <virgiawan150@gmail.com>
2022-08-13 07:30:42 +0700

    [The Router] The final project (simple website with multiple pages): Fetching blog post from spaceflightnews API

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,3 +1,35 @@
+import { useEffect, useState } from 'react';
+
 export default function Blog() {
-  return <h1>Blog page</h1>;
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(function () {
+    async function getArticles() {
+      const request = await fetch('https://api.spaceflightnewsapi.net/v3/articles');
+      const response = await request.json();
+
+      setArticles(response);
+      setLoading(false);
+    }
+    getArticles();
+  }, [])
+
+  return (
+    <section>
+      <h1>Blog page</h1>
+      <p>Berikut ini adalah tulisan-tulisanku (tapi bo'ong): </p>
+      {loading ? <p>Loading articles..</p> : (
+        <div>
+          {
+            articles.map(function(article) {
+              return (
+                <article key={article.id}>{article.title}</article>
+              )
+            })
+          }
+        </div>
+      )}
+    </section>
+  );
 }
